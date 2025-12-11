90ebb92b002feb137ca8f1df1235f7e1ed49aa52
Sasaki Kojiro <132357194+SteinsDevil@users.noreply.github.com>
2025-12-11 15:47:37 +0200

    rounded images and font handlers, alsmost finished MainPage

---
+++ UML/App/View/Screens/MainPage.java
@@ -0,0 +1,35 @@
+package App.View.Screens;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import App.View.View_t;
+import App.View.helper_classes.*;
+import Utils.GlobalConsts;
+
+
+public class MainPage implements View_t{
+
+    final int wWidth = Utils.GlobalConsts.wWidth;
+    final int wHeight = Utils.GlobalConsts.wHeight;
+
+
+    public JLabel title = new JLabel("Καλως ήρθατε");
+    public JLabel title2 = new JLabel("στην Bank of TUC");
+    public JTextArea usrname = new JTextArea(20, 200);
+    public JTextArea pword = new JTextArea(20,200);
+
+
+
+
+
+
+
+
+
+}
